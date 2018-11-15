495dcf91d82cb10fb654a951d320ba24f1916782
Ivan Kurtovic <ivan.kurtovic@pwc.com>
2018-11-15 14:32:10 +0100

    Developed Invoice Entity

---
+++ src/main/java/com/pwc/restaurant/Restaurant/entities/Invoice.java
@@ -0,0 +1,28 @@
+package com.pwc.restaurant.Restaurant.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TBL_INVOICE") 
+public class Invoice extends BaseEntity {
+	
+	private String tableNumber;
+
+	
+	
+	
+	public String getTableNumber() {
+		return tableNumber;
+	}
+
+	public void setTableNumber(String tableNumber) {
+		this.tableNumber = tableNumber;
+	}
+	
+	
+	
+	
+
+}
